2402b5e6ac48618f770ed64c0e558acdc0cff645
Edsel Caprice <caprice@mail.usf.edu>
2018-07-27 10:43:02 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -1,5 +1,5 @@
 def selects_all_female_bears_return_name_and_age
-  "select gender = 'f' from bears group by name, age"
+  "select name from bears where gender = 'f'"
 end
 
 def selects_all_bears_names_and_orders_in_alphabetical_order
